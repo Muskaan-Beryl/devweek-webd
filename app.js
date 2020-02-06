9dcb5f311f1242ad71c8bf03030c1c7393efe332
Muskaan-Beryl <54888357+Muskaan-Beryl@users.noreply.github.com>
2020-02-06 16:32:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ weather.temperature = {
 const KELVIN = 273;
 
 // API KEY
-const key = '57dcf7b099a25a6188050a49b786261a';
+const key = '94ba789e408d5db3f97a8c2de84ce83a';
 // CHECK IF BROWSER SUPPORTS GEOLOCATION
 if('geolocation' in navigator){
     navigator.geolocation.getCurrentPosition(setPosition, showError);
